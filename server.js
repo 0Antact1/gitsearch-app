f6af4fd55c35de8929c574a600b56da4fb723a3e
0Antact1 <95278822+0Antact1@users.noreply.github.com>
2023-03-06 10:13:13 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,100 +1,25 @@
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
+if (process.env.NODE_ENV !== 'prod') {
+    require('dotenv').config()
+}
 
-const app = express();
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const indexRoute = require('./routes/index')
 
-app.use(cors())
-app.use(express.json());
+app.set('view engine', 'ejs')
+app.set('views',__dirname+'/views')
+app.set('layout','layouts/layout')
 
-app.post('/api/github', (req, res) => {
-  res.send('Hello, this is the GitHub API endpoint.');
-});
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL)
 
-app.get('/api/github', async (req, res) => {
-  try {
-    const { orgName, n, m } = req.query;
-    console.log(orgName)
-    console.log(n,m)
-    
-    let page = 1;
-    let allRepos = [];
-  
-    while (true) {
-      const response = await axios.get(`https://api.github.com/orgs/${orgName}/repos?page=${page}&per_page=100`, {
-        headers: { 'User-Agent': 'request' },
-      });
-  
-      const repos = response.data;
-      allRepos = [...allRepos, ...repos];
-  
-      const linkHeader = response.headers.link || '';
-      const nextPageUrl = linkHeader
-        .split(',')
-        .find((link) => link.includes('rel="next"'))
-        ?.match(/<(.+?)>/)?.[1];
-  
-      if (!nextPageUrl) {
-        break;
-      }
-  
-      page++;
-    }
-  
-    allRepos.sort((a, b) => b.forks_count - a.forks_count)
-    // const reposResponse = await axios.get(`https://api.github.com/orgs/${orgName}/repos`);
-    // const repos = reposResponse.data;
-    // repos.sort((a, b) => b.forks - a.forks);
-    const result = [];
+const db = mongoose.connection
+db.on('error', error => console.error('mc'))
+db.once('open', () => console.log('done'))
 
-    for (let i = 0; i < n && i < allRepos.length; i++) {
-      const repo = allRepos[i];
-      // const forkersResponse = await axios.get(repo.forks_url);
-      // const forkers = forkersResponse.data.map(fork => ({
-      //   login: fork.owner.login,
-      //   date: fork.created_at
-      // })).sort((a, b) => new Date(a.date) - new Date(b.date));
-      let page = 1;
-      let allForkers = [];
-      // api rate lim exceeded :( on multiple nets
-      while (true) {
-        const forksUrl = repo.forks_url;
-        const response = await axios.get(`${forksUrl}?page=${page}&per_page=100`, {
-          headers: { 'User-Agent': 'request' },
-        });
-    
-        const forkers = response.data.map(fork => ({
-            login: fork.owner.login,
-            date: fork.created_at
-          }));
-        allForkers = [...allForkers, ...forkers];
-    
-        const linkHeader = response.headers.link || '';
-        const nextPageUrl = linkHeader
-          .split(',')
-          .find((link) => link.includes('rel="next"'))
-          ?.match(/<(.+?)>/)?.[1];
-    
-        if (!nextPageUrl) {
-          break;
-        }
-        page++;
-      }
-      allForkers.sort((a, b) => new Date(a.date) - new Date(b.date));
+app.use(expressLayouts)
+app.use(express.static('public'))
+app.use(indexRoute)
 
-      result.push({
-        name: repo.name,
-        forkers: allForkers.slice(0, m)
-      });
-    }
-
-    res.json(result);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Internal server error');
-  }
-});
-
-app.listen(3000, () => {
-  console.log(`Server listening at http://localhost:3000`);
-});
+app.listen(process.env.PORT || 3000)
